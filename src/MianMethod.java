67bee55e87c54c5730104cdad1431951573e1f63
donna93 <chocolatedonna1993@gmail.com>
2019-05-23 12:53:24 -0400

    backtracking

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,16 +4,10 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-        int [][] matrix = {{1, 2, 2, 3, 5},
-                           {3, 2, 3, 4, 4},
-                           {2, 4, 5, 3, 1},
-                           {6, 7, 1, 4, 5},
-                           {5, 1, 1, 2, 4}};
-
-        PacificAltlanticWaterFlow p = new PacificAltlanticWaterFlow();
-        List<int[]> points = p.pacificAtlantic(matrix);
-        for(int i = 0; i < points.size(); i++){
-            System.out.println(points.get(i)[0]+ " " + points.get(i)[1]);
+        String digits = "23";
+        List<String> combinations = new LetterComOfPhoneNbr().letterCombinations(digits);
+        for(int i = 0; i < combinations.size(); i++){
+            System.out.println(combinations.get(i));
         }
     }
 }
