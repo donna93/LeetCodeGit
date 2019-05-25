98f384526f682a5653fbfd11a66c1fb1748be349
donna93 <chocolatedonna1993@gmail.com>
2019-05-25 16:44:17 -0400

    70.climingStaris-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,20 +4,21 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//* Example:
-// * Input: 4
-//                * Output: [
-// *  [".Q..",  // Solution 1
-// *   "...Q",
-// *   "Q...",
-// *   "..Q."],
-// *
-// *  ["..Q.",  // Solution 2
-// *   "Q...",
-// *   "...Q",
-// *   ".Q.."]
-// * ]
-        List<List<String>> solution = new NQueens().solveNQueens(4);
-        System.out.println(solution.toString());
+//* Example 1:
+// * Input: 2
+//                * Output: 2
+//                * Explanation: There are two ways to climb to the top.
+//                * 1. 1 step + 1 step
+//                * 2. 2 steps
+//                *
+// * Example 2:
+// * Input: 3
+//                * Output: 3
+//                * Explanation: There are three ways to climb to the top.
+//                * 1. 1 step + 1 step + 1 step
+//                * 2. 1 step + 2 steps
+//                * 3. 2 steps + 1 step
+        System.out.println(new ClimingStairs().climbStairs(2));
+        System.out.println(new ClimingStairs().climbStairs(3));
     }
 }
