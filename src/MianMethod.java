1bd5022ea4913a1552285c6332bcd2c20e008469
donna93 <chocolatedonna1993@gmail.com>
2019-05-25 16:56:58 -0400

    198.HouseRobber-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,21 +4,16 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//* Example 1:
-// * Input: 2
-//                * Output: 2
-//                * Explanation: There are two ways to climb to the top.
-//                * 1. 1 step + 1 step
-//                * 2. 2 steps
-//                *
+//   Example 1:
+// * Input: [1,2,3,1]
+// * Output: 4
+//
 // * Example 2:
-// * Input: 3
-//                * Output: 3
-//                * Explanation: There are three ways to climb to the top.
-//                * 1. 1 step + 1 step + 1 step
-//                * 2. 1 step + 2 steps
-//                * 3. 2 steps + 1 step
-        System.out.println(new ClimingStairs().climbStairs(2));
-        System.out.println(new ClimingStairs().climbStairs(3));
+// * Input: [2,7,9,3,1]
+// * Output: 12
+        int[] rob1 = {1,2,3,1};
+        int[] rob2 = {2,7,9,3,1};
+        System.out.println(new HouseRobber().rob(rob1));
+        System.out.println(new HouseRobber().rob(rob2));
     }
 }
