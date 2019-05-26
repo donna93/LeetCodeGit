a75adc513cbba489ba04254c9d1636f807391243
donna93 <chocolatedonna1993@gmail.com>
2019-05-25 22:27:49 -0400

    343. Integer Break - dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,9 +4,16 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//        A = [1, 2, 3, 4]
-// * return: 3,
-        int[] A = {1, 2, 3, 4};
-        System.out.println(new ArithmeticSlices().numberOfArithmeticSlices(A));
+//   Example 1:
+// * Input: 2
+// * Output: 1
+// * Explanation: 2 = 1 + 1, 1 × 1 = 1.
+//                *
+// * Example 2:
+// * Input: 10
+// * Output: 36
+// * Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+        System.out.println(new IntegerBreak().integerBreak(2));
+        System.out.println(new IntegerBreak().integerBreak(10));
     }
 }
