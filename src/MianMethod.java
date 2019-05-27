600d31968abb5612feb9f95f062f36d5cb30373b
donna93 <chocolatedonna1993@gmail.com>
2019-05-27 19:42:14 -0400

    91.DecodeWays-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-// * Example 1:
-// * Input: n = 12
-// * Output: 3
-// * Explanation: 12 = 4 + 4 + 4.
+//        Example 1:
+// * Input: "12"
+//                * Output: 2
+//                * Explanation: It could be decoded as "AB" (1 2) or "L" (12).
 //                *
 // * Example 2:
-// * Input: n = 13
-// * Output: 2
-// * Explanation: 13 = 4 + 9.
-        System.out.println(new PerfectSquares().numSquares(13));
+// * Input: "226"
+//                * Output: 3
+        System.out.println(new DecodeWays().numDecodings("12"));
+        System.out.println(new DecodeWays().numDecodings("226"));
     }
 }
