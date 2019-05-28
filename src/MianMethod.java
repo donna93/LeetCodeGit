ce10db2908b56ebbb157a2db193d0958bd61a0b2
donna93 <chocolatedonna1993@gmail.com>
2019-05-28 17:24:04 -0400

    494.TargetSum-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,17 +5,9 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 //        Example 1:
-// * Input: [1, 5, 11, 5]
-// * Output: true
-//                * Explanation: The array can be partitioned as [1, 5, 5] and [11].
-//                *
-// *
-// * Example 2:
-// * Input: [1, 2, 3, 5]
-// * Output: false
-        int[] input1 = {1, 5, 11, 5};
-        int[] input2 = {1, 2, 3, 5};
-        System.out.println(new PartitionEqualSubsetSum().canPartition(input1));
-        System.out.println(new PartitionEqualSubsetSum().canPartition(input2));
+// * Input: nums is [1, 1, 1, 1, 1], S is 3.
+//                * Output: 5
+        int[] input = {1, 1, 1, 1, 1};
+        System.out.println(new TargetSum().findTargetSumWays(input, 3));
     }
 }
