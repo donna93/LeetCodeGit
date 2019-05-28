e74d9517cb0980dd282a533bcc2375478e77493a
donna93 <chocolatedonna1993@gmail.com>
2019-05-28 16:27:09 -0400

    416.PartitionEqualSubsetSum-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,17 +4,18 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//   Example 1:
-// * Input: [1,7,4,9,2,5]
-// * Output: 6
-// * Explanation: The entire sequence is a wiggle sequence.
+//        Example 1:
+// * Input: [1, 5, 11, 5]
+// * Output: true
+//                * Explanation: The array can be partitioned as [1, 5, 5] and [11].
+//                *
 // *
 // * Example 2:
-// * Input: [1,17,5,10,13,15,10,5,16,8]
-// * Output: 7
-        int[] input = {1,7,4,9,2,5};
-        int[] input2 = {1,17,5,10,13,15,10,5,16,8};
-        System.out.println(new WiggleSubsequence().wiggleMaxLength(input));
-        System.out.println(new WiggleSubsequence().wiggleMaxLength(input2));
+// * Input: [1, 2, 3, 5]
+// * Output: false
+        int[] input1 = {1, 5, 11, 5};
+        int[] input2 = {1, 2, 3, 5};
+        System.out.println(new PartitionEqualSubsetSum().canPartition(input1));
+        System.out.println(new PartitionEqualSubsetSum().canPartition(input2));
     }
 }
