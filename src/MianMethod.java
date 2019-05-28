e5a5627db0e2a77b5a072c070bed22ac78519f97
donna93 <chocolatedonna1993@gmail.com>
2019-05-28 17:50:41 -0400

    139.WordBreak-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,9 +5,23 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 //        Example 1:
-// * Input: nums is [1, 1, 1, 1, 1], S is 3.
-//                * Output: 5
-        int[] input = {1, 1, 1, 1, 1};
-        System.out.println(new TargetSum().findTargetSumWays(input, 3));
+// * Input: s = "leetcode", wordDict = ["leet", "code"]
+// * Output: true
+//                * Explanation: Return true because "leetcode" can be segmented as "leet code".
+//                *
+// * Example 2:
+// * Input: s = "applepenapple", wordDict = ["apple", "pen"]
+// * Output: true
+//                * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+//                *              Note that you are allowed to reuse a dictionary word.
+//                *
+// * Example 3:
+// * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+// * Output: false
+        String s1 = "leetcode";
+        List<String> word1 = new ArrayList<>();
+        word1.add("leet");
+        word1.add("code");
+        System.out.println(new WordBreak().wordBreak(s1, word1));
     }
 }
