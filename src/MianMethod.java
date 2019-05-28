f402333a935f9b3bba4a55a0428fd7032a5f530e
donna93 <chocolatedonna1993@gmail.com>
2019-05-28 19:17:21 -0400

    474.OnesAndZeros-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,24 +4,18 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//        Example 1:
-// * Input: s = "leetcode", wordDict = ["leet", "code"]
-// * Output: true
-//                * Explanation: Return true because "leetcode" can be segmented as "leet code".
-//                *
+//   Example 1:
+// * Input: Array = {"10", "0001", "111001", "1", "0"}, m = 5, n = 3
+// * Output: 4
+// * Explanation: This are totally 4 strings can be formed by the using of 5 0s and 3 1s, which are “10,”0001”,”1”,”0”
+// *
+// *
 // * Example 2:
-// * Input: s = "applepenapple", wordDict = ["apple", "pen"]
-// * Output: true
-//                * Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
-//                *              Note that you are allowed to reuse a dictionary word.
-//                *
-// * Example 3:
-// * Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
-// * Output: false
-        String s1 = "leetcode";
-        List<String> word1 = new ArrayList<>();
-        word1.add("leet");
-        word1.add("code");
-        System.out.println(new WordBreak().wordBreak(s1, word1));
+// * Input: Array = {"10", "0", "1"}, m = 1, n = 1
+// * Output: 2
+        String[] strs = {"10", "0001", "111001", "1", "0"};
+        String[] strs2 = {"10", "0", "1"};
+        System.out.println(new OnesAndZeros().findMaxForm(strs, 5, 3));
+        System.out.println(new OnesAndZeros().findMaxForm(strs2, 1, 1));
     }
 }
