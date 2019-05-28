0f23446d8370558fd919c4058485fe53d89bb056
donna93 <chocolatedonna1993@gmail.com>
2019-05-28 13:44:12 -0400

    376.WiggleSubsequences-dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,9 +5,16 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 //   Example 1:
-// * Input: [[1,2], [2,3], [3,4]]
-// * Output: 2
-        int[][] paris = {{1, 2}, {2, 3}, {3, 4}};
-        System.out.println(new MaxLenOfPairChain().findLongestChain(paris));
+// * Input: [1,7,4,9,2,5]
+// * Output: 6
+// * Explanation: The entire sequence is a wiggle sequence.
+// *
+// * Example 2:
+// * Input: [1,17,5,10,13,15,10,5,16,8]
+// * Output: 7
+        int[] input = {1,7,4,9,2,5};
+        int[] input2 = {1,17,5,10,13,15,10,5,16,8};
+        System.out.println(new WiggleSubsequence().wiggleMaxLength(input));
+        System.out.println(new WiggleSubsequence().wiggleMaxLength(input2));
     }
 }
