4cc11bdcf29d19f590b4c6aef4478f3c06b3fa45
donna93 <chocolatedonna1993@gmail.com>
2019-05-29 12:12:30 -0400

    123.BestTimeToBuySellStock - dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,9 +5,21 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 //* Example 1:
-//* Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
-//* Output: 8
-        int[] input = {1, 3, 2, 8, 4, 9};
-        System.out.println(new BestTimeBuySellStockWithFee().maxProfit(input, 2));
+// * Input: [3,3,5,0,0,3,1,4]
+// * Output: 6
+
+// * Example 2:
+// * Input: [1,2,3,4,5]
+// * Output: 4
+// *
+// * Example 3:
+// * Input: [7,6,4,3,1]
+// * Output: 0
+        int[] input1 = {3,3,5,0,0,3,1,4};
+        int[] input2 = {1,2,3,4,5};
+        int[] input3 = {7,6,4,3,1};
+        System.out.println(new BestTimeToBuySellStockIII().maxProfit(input1));
+        System.out.println(new BestTimeToBuySellStockIII().maxProfit(input2));
+        System.out.println(new BestTimeToBuySellStockIII().maxProfit(input3));
     }
 }
