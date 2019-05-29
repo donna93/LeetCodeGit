895542aa51ff572b4dfe4002d2ed708bf6d64553
donna93 <chocolatedonna1993@gmail.com>
2019-05-29 10:35:40 -0400

    714.Best Time to Buy and Sell Stock with Transaction Fee - dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//   Example:
-// * Input: [1,2,3,0,2]
-// * Output: 3
-        int[] input = {1, 2, 3, 0, 2};
-        System.out.println(new BestTimeBuySellStockCooldown().maxProfit(input));
+//* Example 1:
+//* Input: prices = [1, 3, 2, 8, 4, 9], fee = 2
+//* Output: 8
+        int[] input = {1, 3, 2, 8, 4, 9};
+        System.out.println(new BestTimeBuySellStockWithFee().maxProfit(input, 2));
     }
 }
