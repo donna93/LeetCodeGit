e2f9efd4f6498a5c446d6d5cfc26bf21220feb31
donna93 <chocolatedonna1993@gmail.com>
2019-05-30 13:21:55 -0400

    405.Convert Nbr To Hexadecimal - Math

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,13 +5,18 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 // * Example 1:
-// * Input: 100
-//                * Output: "202"
+// * Input:
+// * 26
+// * Output:
+// * "1a"
 //                *
 // * Example 2:
-// * Input: -7
-//                * Output: "-10"
-        System.out.println(new Base7().convertToBase7(100));
-        System.out.println(new Base7().convertToBase7(-7));
+// * Input:
+// * -1
+// * Output:
+// * "ffffffff"
+
+        System.out.println(new ConvertNbrToHexadecimal().toHex(26));
+        System.out.println(new Base7().convertToBase7(-1));
     }
 }
