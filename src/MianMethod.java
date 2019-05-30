1a1284bb94a66e1b54777d553c4f5b490e323987
donna93 <chocolatedonna1993@gmail.com>
2019-05-29 21:18:28 -0400

    72.EditDistance - dp

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,8 +5,17 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 //        Example 1:
-// * Input: "sea", "eat"
-//                * Output: 2
-        System.out.println(new DeleteOperationForTwoStr().minDistance("sea", "eat"));
+// * Input: word1 = "horse", word2 = "ros"
+//                * Output: 3
+//                * Explanation:
+// * horse -> rorse (replace 'h' with 'r')
+// * rorse -> rose (remove 'r')
+// * rose -> ros (remove 'e')
+// *
+// * Example 2:
+// * Input: word1 = "intention", word2 = "execution"
+// * Output: 5
+        System.out.println(new EditDistance().minDistance("horse", "ros"));
+        System.out.println(new EditDistance().minDistance("intention", "execution"));
     }
 }
