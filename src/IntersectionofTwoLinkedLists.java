ac20daa949cf987c2ea77e5320cb8c1bf129c4f2
donna93 <chocolatedonna1993@gmail.com>
2019-05-31 11:21:20 -0400

    160.Intersection Of Two LinkedList

---
+++ src/IntersectionofTwoLinkedLists.java
@@ -0,0 +1,13 @@
+/**
+ * Write a program to find the node at which the intersection of two singly linked lists begins.
+ */
+public class IntersectionofTwoLinkedLists {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode l1 = headA, l2 = headB;
+        while (l1 != l2){
+            l1 = (l1 == null) ? headB : l1.next;
+            l2 = (l2 == null) ? headA : l2.next;
+        }
+        return l1;
+    }
+}
