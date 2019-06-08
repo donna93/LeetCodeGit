9ed86a533ebae9dc51f30d5cb03300c6c3e7a5d1
donna93 <chocolatedonna1993@gmail.com>
2019-06-07 22:18:55 -0400

    769.Max Chunks to make sorted - array

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//        Example 1:
-// * Input: A = [5,4,0,3,1,6,2]
+//* Example 1:
+// * Input: arr = [4,3,2,1,0]
+// * Output: 1
+//                * Explanation:
+// * Splitting into two or more chunks will not return the required result.
+// * For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0, 1, 2], which isn't sorted.
+//                *
+// * Example 2:
+// * Input: arr = [1,0,2,3,4]
 // * Output: 4
-        int[] input = {5,4,0,3,1,6,2};
-        System.out.println(new ArrayNesting().arrayNesting(input));
+        int[] input1 = {4,3,2,1,0};
+        int[] input2 = {1,0,2,3,4};
+        System.out.println(new MaxChunksToMakeSorted().maxChunksToSorted(input1));
+        System.out.println(new MaxChunksToMakeSorted().maxChunksToSorted(input2));
     }
 }
