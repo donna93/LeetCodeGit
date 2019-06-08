043ae7abfd08e30f9a9d3c39db132997f1381a2b
donna93 <chocolatedonna1993@gmail.com>
2019-06-07 20:34:40 -0400

    766.Toeplitz Matrix - array

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,20 +5,31 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 //        Example 1:
-// * Input: [1, 2, 2, 3, 1]
-// * Output: 2
+// * Input:
+// * matrix = [
+// *   [1,2,3,4],
+// *   [5,1,2,3],
+// *   [9,5,1,2]
+// * ]
+// * Output: True
 //                * Explanation:
-// * The input array has a degree of 2 because both elements 1 and 2 appear twice.
-// * Of the subarrays that have the same degree:
-// * [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2, 2]
-// * The shortest length is 2. So return 2.
-//                *
+// * In the above grid, the diagonals are:
+// * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+//                * In each diagonal all elements are the same, so the answer is True.
+// *
 // * Example 2:
-// * Input: [1,2,2,3,1,4,2]
-// * Output: 6
-        int[] input1 = {1, 2, 2, 3, 1};
-        int[] input2 = {1,2,2,3,1,4,2};
-        System.out.println(new DegreeOfArray().findShortestSubArray(input1));
-        System.out.println(new DegreeOfArray().findShortestSubArray(input2));
+// * Input:
+// * matrix = [
+// *   [1,2],
+// *   [2,2]
+// * ]
+// * Output: False
+        int[][] matrix1 = {{1,2,3,4},
+                            {5,1,2,3},
+                            {9,5,1,2}};
+        int[][] matrix2 = {{1, 2},
+                           {2, 2}};
+        System.out.println(new ToeplitzMatrix().isToeplitzMatrix(matrix1));
+        System.out.println(new ToeplitzMatrix().isToeplitzMatrix(matrix2));
     }
 }
