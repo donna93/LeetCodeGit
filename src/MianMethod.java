d098a164f134164d57173f11dd39929f63b9497e
donna93 <chocolatedonna1993@gmail.com>
2019-06-08 14:40:58 -0400

    210.Course Schedule II - graph

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,18 +4,22 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//   Example 1:
+//        Example 1:
 // * Input: 2, [[1,0]]
-// * Output: true
-// * Explanation: There are a total of 2 courses to take.
-// *              To take course 1 you should have finished course 0. So it is possible.
+// * Output: [0,1]
+// * Explanation: There are a total of 2 courses to take. To take course 1 you should have finished
+// *              course 0. So the correct course order is [0,1] .
 // *
 // * Example 2:
-// * Input: 2, [[1,0],[0,1]]
-// * Output: false
+// * Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+// * Output: [0,1,2,3] or [0,2,1,3]
         int[][] matrix1 = {{1, 0}};
-        int[][] matrix2 = {{1, 0},{0, 1}};
-        System.out.println(new CourseSchedule().canFinish(2, matrix1));
-        System.out.println(new CourseSchedule().canFinish(2, matrix2));
+        int[][] matrix2 = {{1, 0},{2, 0},{3, 1},{3, 2}};
+
+        int[] s1 = new CourseScheduleII().findOrder(2, matrix1);
+        System.out.println(Arrays.toString(s1));
+
+        int[] s2 = new CourseScheduleII().findOrder(4, matrix2);
+        System.out.println(Arrays.toString(s2));
     }
 }
