6c7dff09debd3ec7115c096a966cc371afef9d20
donna93 <chocolatedonna1993@gmail.com>
2019-06-07 21:33:30 -0400

    565.Array nesting - array

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,31 +5,9 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 //        Example 1:
-// * Input:
-// * matrix = [
-// *   [1,2,3,4],
-// *   [5,1,2,3],
-// *   [9,5,1,2]
-// * ]
-// * Output: True
-//                * Explanation:
-// * In the above grid, the diagonals are:
-// * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
-//                * In each diagonal all elements are the same, so the answer is True.
-// *
-// * Example 2:
-// * Input:
-// * matrix = [
-// *   [1,2],
-// *   [2,2]
-// * ]
-// * Output: False
-        int[][] matrix1 = {{1,2,3,4},
-                            {5,1,2,3},
-                            {9,5,1,2}};
-        int[][] matrix2 = {{1, 2},
-                           {2, 2}};
-        System.out.println(new ToeplitzMatrix().isToeplitzMatrix(matrix1));
-        System.out.println(new ToeplitzMatrix().isToeplitzMatrix(matrix2));
+// * Input: A = [5,4,0,3,1,6,2]
+// * Output: 4
+        int[] input = {5,4,0,3,1,6,2};
+        System.out.println(new ArrayNesting().arrayNesting(input));
     }
 }
