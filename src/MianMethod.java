ffb8223805499b246a375bdae9c2449ca9c44a89
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 11:22:24 -0400

    136.SIngles number - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-///Example:
-// * Input: x = 1, y = 4
-// * Output: 2
-        System.out.println(new HammingDistance().hammingDistance(1, 4));
-        System.out.println(new HammingDistance().hammingDistance2(1, 4));
-        System.out.println(new HammingDistance().hammingDistance3(1, 4));
+//* Example 1:
+// * Input: [2,2,1]
+// * Output: 1
+//                *
+// * Example 2:
+// * Input: [4,1,2,1,2]
+// * Output: 4
+        int[] num1 = {2,2,1};
+        int[] num2 = {4,1,2,1,2};
+        System.out.println(new SingleNbr().singleNumber(num1));
+        System.out.println(new SingleNbr().singleNumber(num2));
     }
 }
