2ab6cdf4e5c28bbba037c2ef9f9b9ed80095c920
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 19:09:13 -0400

    371.Sum Of Two Integers - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,21 +4,14 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//   Example 1:
-// * Input: 5
-// * Output: 2
-// * Explanation: The binary representation of 5 is 101 (no leading zero bits),
-// * and its complement is 010. So you need to output 2.
+//* Example 1:
+// * Input: a = 1, b = 2
+//                * Output: 3
 //                *
 // * Example 2:
-// * Input: 1
-// * Output: 0
-        System.out.println(new NumberComplement().findComplement(5));
-        System.out.println(new NumberComplement().findComplement2(5));
-        System.out.println(new NumberComplement().findComplement3(5));
-
-        System.out.println(new NumberComplement().findComplement(1));
-        System.out.println(new NumberComplement().findComplement2(1));
-        System.out.println(new NumberComplement().findComplement3(1));
+// * Input: a = -2, b = 3
+//                * Output: 1
+        System.out.println(new SumOfTwoIntegers().getSum(1, 2));
+        System.out.println(new SumOfTwoIntegers().getSum(-2, 3));
     }
 }
