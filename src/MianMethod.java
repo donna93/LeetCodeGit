6309f7777b42bd80f861ed90870a1aef0eb692c2
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 19:34:09 -0400

    318.Max Product Of Word Length - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,14 +4,25 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//* Example 1:
-// * Input: a = 1, b = 2
-//                * Output: 3
+//        Example 1:
+// * Input: ["abcw","baz","foo","bar","xtfn","abcdef"]
+// * Output: 16
+//                * Explanation: The two words can be "abcw", "xtfn".
 //                *
 // * Example 2:
-// * Input: a = -2, b = 3
-//                * Output: 1
-        System.out.println(new SumOfTwoIntegers().getSum(1, 2));
-        System.out.println(new SumOfTwoIntegers().getSum(-2, 3));
+// * Input: ["a","ab","abc","d","cd","bcd","abcd"]
+// * Output: 4
+//                * Explanation: The two words can be "ab", "cd".
+//                *
+// * Example 3:
+// * Input: ["a","aa","aaa","aaaa"]
+// * Output: 0
+        String[] input1 = {"abcw","baz","foo","bar","xtfn","abcdef"};
+        String[] input2 = {"a","ab","abc","d","cd","bcd","abcd"};
+        String[] input3 = {"a","aa","aaa","aaaa"};
+
+        System.out.println(new MaxProductOfWordLen().maxProduct(input1));
+        System.out.println(new MaxProductOfWordLen().maxProduct(input2));
+        System.out.println(new MaxProductOfWordLen().maxProduct(input3));
     }
 }
