82752ae53a96e6764e149e306ef1a68c29312a8f
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 19:53:34 -0400

    338.Counting Bits - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,25 +4,17 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//        Example 1:
-// * Input: ["abcw","baz","foo","bar","xtfn","abcdef"]
-// * Output: 16
-//                * Explanation: The two words can be "abcw", "xtfn".
-//                *
+// * Example 1:
+// * Input: 2
+//                * Output: [0,1,1]
+// *
 // * Example 2:
-// * Input: ["a","ab","abc","d","cd","bcd","abcd"]
-// * Output: 4
-//                * Explanation: The two words can be "ab", "cd".
-//                *
-// * Example 3:
-// * Input: ["a","aa","aaa","aaaa"]
-// * Output: 0
-        String[] input1 = {"abcw","baz","foo","bar","xtfn","abcdef"};
-        String[] input2 = {"a","ab","abc","d","cd","bcd","abcd"};
-        String[] input3 = {"a","aa","aaa","aaaa"};
+// * Input: 5
+//                * Output: [0,1,1,2,1,2]
+        int[] output1 = new CountingBits().countBits(2);
+        System.out.println(Arrays.toString(output1));
 
-        System.out.println(new MaxProductOfWordLen().maxProduct(input1));
-        System.out.println(new MaxProductOfWordLen().maxProduct(input2));
-        System.out.println(new MaxProductOfWordLen().maxProduct(input3));
+        int[] output2 = new CountingBits().countBits(5);
+        System.out.println(Arrays.toString(output2));
     }
 }
