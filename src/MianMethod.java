e06e83c8c1e89306708a611774835b1481eceff5
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 11:45:23 -0400

    268.Missing Nbr - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-//* Example 1:
-// * Input: [2,2,1]
-// * Output: 1
+//        Example 1:
+// * Input: [3,0,1]
+// * Output: 2
 //                *
 // * Example 2:
-// * Input: [4,1,2,1,2]
-// * Output: 4
-        int[] num1 = {2,2,1};
-        int[] num2 = {4,1,2,1,2};
-        System.out.println(new SingleNbr().singleNumber(num1));
-        System.out.println(new SingleNbr().singleNumber(num2));
+// * Input: [9,6,4,2,3,5,7,0,1]
+// * Output: 8
+        int[] num1 = {3,0,1};
+        int[] num2 = {9,6,4,2,3,5,7,0,1};
+        System.out.println(new MissingNumber().missingNumber(num1));
+        System.out.println(new MissingNumber().missingNumber(num2));
     }
 }
