6c049b6fba954aaa89704b133f9d1344e582ab41
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 18:42:23 -0400

    476.Number Complement - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,31 +4,21 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-// * Example 1:
+//   Example 1:
 // * Input: 5
-// * Output: True
-// * Explanation:
-// * The binary representation of 5 is: 101
+// * Output: 2
+// * Explanation: The binary representation of 5 is 101 (no leading zero bits),
+// * and its complement is 010. So you need to output 2.
 //                *
 // * Example 2:
-// * Input: 7
-// * Output: False
-// * Explanation:
-// * The binary representation of 7 is: 111.
-// *
-// * Example 3:
-// * Input: 11
-// * Output: False
-// * Explanation:
-// * The binary representation of 11 is: 1011.
-//                *
-// * Example 4:
-// * Input: 10
-// * Output: True
-// * Explanation:
-        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(5));
-        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(7));
-        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(11));
-        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(10));
+// * Input: 1
+// * Output: 0
+        System.out.println(new NumberComplement().findComplement(5));
+        System.out.println(new NumberComplement().findComplement2(5));
+        System.out.println(new NumberComplement().findComplement3(5));
+
+        System.out.println(new NumberComplement().findComplement(1));
+        System.out.println(new NumberComplement().findComplement2(1));
+        System.out.println(new NumberComplement().findComplement3(1));
     }
 }
