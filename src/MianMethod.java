55f2622f0740c115c209fa1ca2b6aaf77b90bb21
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 11:16:33 -0400

    461.Hamming Distance - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -4,32 +4,11 @@ import java.util.List;
 
 public class MianMethod {
     public static void main(String[] args){
-// * Example 1:
-// * Input: [[1,2], [1,3], [2,3]]
-// * Output: [2,3]
-// * Explanation: The given undirected graph will be like this:
-// *   1
-//                *  / \
-// * 2 - 3
-//                *
-// * Example 2:
-// * Input: [[1,2], [2,3], [3,4], [1,4], [1,5]]
-// * Output: [1,4]
-        int[][] input1 = {
-                {1, 2},
-                {1, 3},
-                {2, 3}
-        };
-        int[][] input2 = {
-                {1, 2},
-                {2, 3},
-                {3, 4},
-                {1, 4},
-                {1, 5}
-        };
-        int[] out1 = new RedundantConnection().findRedundantConnection(input1);
-        int[] outf2 = new RedundantConnection().findRedundantConnection(input2);
-        System.out.println(Arrays.toString(out1));
-        System.out.println(Arrays.toString(outf2));
+///Example:
+// * Input: x = 1, y = 4
+// * Output: 2
+        System.out.println(new HammingDistance().hammingDistance(1, 4));
+        System.out.println(new HammingDistance().hammingDistance2(1, 4));
+        System.out.println(new HammingDistance().hammingDistance3(1, 4));
     }
 }
