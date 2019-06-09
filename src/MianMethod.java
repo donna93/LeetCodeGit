2414cc90a6260884b55ce40b3a4de817f5a2123c
donna93 <chocolatedonna1993@gmail.com>
2019-06-09 14:50:29 -0400

    693.Binary Number With Alternating Bits - 位运算

--- src/MianMethod.java
+++ src/MianMethod.java
@@ -5,16 +5,30 @@ import java.util.List;
 public class MianMethod {
     public static void main(String[] args){
 // * Example 1:
-// * Input: 16
-// * Output: true
-// *
-// * Example 2:
 // * Input: 5
-// * Output: false
-        System.out.println(new PowerOfFour().isPowerOfFour(16));
-        System.out.println(new PowerOfFour().isPowerOfFour2(16));
-
-        System.out.println(new PowerOfFour().isPowerOfFour(5));
-        System.out.println(new PowerOfFour().isPowerOfFour2(5));
+// * Output: True
+// * Explanation:
+// * The binary representation of 5 is: 101
+//                *
+// * Example 2:
+// * Input: 7
+// * Output: False
+// * Explanation:
+// * The binary representation of 7 is: 111.
+// *
+// * Example 3:
+// * Input: 11
+// * Output: False
+// * Explanation:
+// * The binary representation of 11 is: 1011.
+//                *
+// * Example 4:
+// * Input: 10
+// * Output: True
+// * Explanation:
+        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(5));
+        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(7));
+        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(11));
+        System.out.println(new BinaryNbrWithAlternatingBits().hasAlternatingBits(10));
     }
 }
